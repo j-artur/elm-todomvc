8d742d27d119ce65a586cba25b4999d8495541d9
j-artur <jartur.dev@gmail.com>
2022-11-16 21:01:37 -0300

    name type

--- src/Main.elm
+++ src/Main.elm
@@ -193,7 +193,7 @@ view model =
         , footer [ class "info" ]
             [ p [] [ text "Double-click to edit a todo" ]
             , p [] [ text "Written by ", a [ href "https://github.com/evancz" ] [ text "Evan Czaplicki" ] ]
-            , p [] [ text "Forked by ", a [ href "https://github.com/j-artur" ] [ text "João Czaplicki" ] ]
+            , p [] [ text "Forked by ", a [ href "https://github.com/j-artur" ] [ text "João Artur" ] ]
             , p [] [ text "Part of ", a [ href "http://todomvc.com" ] [ text "TodoMVC" ] ]
             ]
         ]
